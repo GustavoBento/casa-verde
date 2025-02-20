663a3bb9e0748174ad2f1ce142c8001379e51b17
GustavoBento <gustavo_grb-@hotmail.com>
2025-02-20 20:30:20 -0300

    feat: estilizando o menu

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,9 +1,17 @@
 import logo from "assets/logo.png";
+import styles from "./Menu.module.css"
 
 export default function Menu() {
   return (
-    <header>
+    <header className={styles.cabecalho}>
       <img src={logo} alt="Logo da casa verde." />
+      <nav className={styles.menu}>
+            <a href="/" className={styles.menu__item}>Como fazer <span className={styles.menu__item__barra}>/</span></a>
+            <a href="/" className={styles.menu__item}>Ofertas <span className={styles.menu__item__barra}>/</span></a>
+            <a href="/" className={styles.menu__item}>Depoimentos <span className={styles.menu__item__barra}>/</span></a>
+            <a href="/" className={styles.menu__item}>Vídeos <span className={styles.menu__item__barra}>/</span></a>
+            <a href="/" className={styles.menu__item}>Meu carrinho</a>
+      </nav>
     </header>
   );
 }

8594c33581c21092b58ae39f4031e29e9b1f84fc
GustavoBento <gustavo_grb-@hotmail.com>
2025-03-01 19:13:29 -0300

    Feat: Ajeitando o menu para mobile

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -2,16 +2,28 @@ import styles from "./Menu.module.css";
 import logo from "assets/images/logo.png";
 
 export default function Menu() {
+
   return (
     <header className={styles.cabecalho}>
       <img src={logo} alt="Logo da casa verde." />
       <nav className={styles.menu}>
-            <a href="/" className={styles.menu__item}>Como fazer <span className={styles.menu__item__barra}>/</span></a>
-            <a href="/" className={styles.menu__item}>Ofertas <span className={styles.menu__item__barra}>/</span></a>
-            <a href="/" className={styles.menu__item}>Depoimentos <span className={styles.menu__item__barra}>/</span></a>
-            <a href="/" className={styles.menu__item}>Vídeos <span className={styles.menu__item__barra}>/</span></a>
-            <a href="/" className={styles.menu__item}>Meu carrinho</a>
+        <a href="/" className={styles.menu__item}>
+          Como fazer <span className={styles.menu__item__barra}>/</span>
+        </a>
+        <a href="/" className={styles.menu__item}>
+          Ofertas <span className={styles.menu__item__barra}>/</span>
+        </a>
+        <a href="/" className={styles.menu__item}>
+          Depoimentos <span className={styles.menu__item__barra}>/</span>
+        </a>
+        <a href="/" className={styles.menu__item}>
+          Vídeos <span className={styles.menu__item__barra}>/</span>
+        </a>
+        <a href="/" className={styles.menu__item}>
+          Meu carrinho
+        </a>
       </nav>
+      <span className={`material-symbols-outlined ${styles.btn__menu}`} >menu</span>
     </header>
   );
 }

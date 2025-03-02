7ce4fa74d78616aef909b0ab19ecac2d42899f3e
GustavoBento <gustavo_grb-@hotmail.com>
2025-03-02 17:37:05 -0300

    Feat: Ajeitando a seção do menu, estilizando para o mobile

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,29 +1,58 @@
+import { useState } from "react";
 import styles from "./Menu.module.css";
 import logo from "assets/images/logo.png";
 
 export default function Menu() {
+  const [menu, setMenu] = useState<boolean>(false);
+
+  function abrirMenu() {
+    setMenu(true);
+  }
+
+  function fecharMenu() {
+    setMenu(false);
+  }
+  console.log(menu);
 
   return (
     <header className={styles.cabecalho}>
       <img src={logo} alt="Logo da casa verde." />
-      <nav className={styles.menu}>
-        <a href="/" className={styles.menu__item}>
-          Como fazer <span className={styles.menu__item__barra}>/</span>
-        </a>
-        <a href="/" className={styles.menu__item}>
-          Ofertas <span className={styles.menu__item__barra}>/</span>
-        </a>
-        <a href="/" className={styles.menu__item}>
-          Depoimentos <span className={styles.menu__item__barra}>/</span>
-        </a>
-        <a href="/" className={styles.menu__item}>
-          Vídeos <span className={styles.menu__item__barra}>/</span>
-        </a>
-        <a href="/" className={styles.menu__item}>
-          Meu carrinho
-        </a>
+      <nav className={`${styles.menu} ${menu ? styles.active : ""}  `}>
+        <ul>
+          <li className={styles.menu__item}>
+            <a href="/">Como fazer</a>
+          </li>
+          <li className={styles.menu__item}>
+            <a href="/">Ofertas</a>
+          </li>
+          <li className={styles.menu__item}>
+            <a href="/">Depoimentos</a>
+          </li>
+          <li className={styles.menu__item}>
+            <a href="/">Vídeos</a>
+          </li>
+          <li className={styles.menu__item}>
+            <a href="/">Meu carrinho</a>
+          </li>
+        </ul>
       </nav>
-      <span className={`material-symbols-outlined ${styles.btn__menu}`} >menu</span>
+      <span
+        /* style={{ display: menu ? "none" : "block" }} */
+        onClick={abrirMenu}
+        className={`material-symbols-outlined ${styles.btn__menu} ${menu ? "" : styles.active}`}
+      >
+        menu
+      </span>
+      <span
+        style={{ display: menu ? "block" : "none" }}
+        className={`material-symbols-outlined ${styles.btn__menu} ${
+          menu ? styles.fechar : ""
+        }`}
+        onClick={fecharMenu}
+      >
+        close
+      </span>
+      <div className={menu ? styles.overlay__ativo : styles.overlay}></div>
     </header>
   );
 }
